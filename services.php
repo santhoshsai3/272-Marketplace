de5554ad96d4861526643552a3caeba6949f2007
Santhosh <santhoshsai3@gmail.com>
2021-12-11 16:26:42 -0800

    print cookie

--- services.php
+++ services.php
@@ -253,6 +253,7 @@ if(isset($_POST['buttonStumps'])) {
     setcookie("buttonStumps", "Stumps", time()+30*24*60*60);
 }
 if(isset($_POST['buttonSubmit'])) {
+    print_r($_COOKIE)
     if(isset($_COOKIE)){
         $lastFive = array_slice($_COOKIE, -5);
         // print_r($lastFive);
